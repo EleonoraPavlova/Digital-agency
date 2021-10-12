74e66f080a6c515faa479ed405faaf031ffe1764
eleonora <nebrudko18@gmail.com>
2021-10-12 20:55:17 +0300

    fix section

---
+++ src/components/home/LastWorks/index.js
@@ -0,0 +1,9 @@
+import './index.scss';
+
+export default function LastWorks() {
+    return <section className="works wrapper">
+        <div className="works__title">
+            <h4 className="text-title">Our Recent Projects</h4>
+        </div>
+    </section>
+}
