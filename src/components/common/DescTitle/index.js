326aad67b3857c07ec2142d6957f57b60d0a4443
eleonora <nebrudko18@gmail.com>
2021-11-01 17:41:13 +0200

    fix

--- src/components/common/DescTitle/index.js
+++ src/components/common/DescTitle/index.js
@@ -1,7 +1,7 @@
 import './index.scss';
 
 export default function DescTitle(props) {
-    return <div className="desc__title">
+    return <div className={`desc__title ${props.additionalClasses}`}>
         <h3 className="text-title desc__title-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h3>
     </div>
 }
